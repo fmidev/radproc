4e9c9eb926ee31d06ae75c67f5b52f7bbb411cb0
Jussi Tiira <jussi.tiira@fmi.fi>
2023-02-10 16:57:44 +0200

    melting example with plots

--- scripts/dev.py
+++ scripts/dev.py
@@ -4,13 +4,20 @@ import pyart
 import matplotlib.pyplot as plt
 
 
-def plot_pseudo_rhi(radar, direction=270, **kws):
+def plot_pseudo_rhi(radar, what='reflectivity_horizontal', direction=270, **kws):
     fig, ax = plt.subplots()
     xsect = pyart.util.cross_section_ppi(radar, [direction])
     display = pyart.graph.RadarDisplay(xsect)
-    display.plot("reflectivity_horizontal", 0, ax=ax, **kws)
+    display.plot(what, 0, ax=ax, **kws)
     ax.set_ylim(bottom=0, top=11)
     ax.set_xlim(left=0, right=150)
+    return ax
+
+
+def plot_ppi(radar, sweep=0, what='reflectivity_horizontal', **kws):
+    fig, ax = plt.subplots()
+    ppi = pyart.graph.RadarDisplay(radar)
+    ppi.plot(what, sweep, ax=ax, **kws)
     return ax
 
 
@@ -28,9 +35,12 @@ def read_h5(filename, exclude_datasets=['dataset13'], **kws):
 
 
 if __name__ == '__main__':
+    plt.close('all')
     datadir = os.path.expanduser('~/data/pvol/')
     f_nomelt1 = os.path.join(datadir, '202302051845_fivih_PVOL.h5')
-    f_melt1 = os.path.join(datadir, '202204040655_fivih_PVOL.h5')
+    # melting at 1-2km
+    f_melt1 = os.path.join(datadir, '202206030010_fivih_PVOL.h5')
     r_nomelt1 = read_h5(f_nomelt1)
     r_melt1 = read_h5(f_melt1)
-    ax = plot_pseudo_rhi(r_melt1, direction=0)
+    ax = plot_pseudo_rhi(r_melt1, what='cross_correlation_ratio', direction=90)
+    ax = plot_ppi(r_melt1, sweep=2, title_flag=False)
