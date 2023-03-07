8d7231acfe40c4d437587494046f9510dea14ce7
Jussi Tiira <jussi.tiira@fmi.fi>
2023-03-07 16:14:10 +0200

    use hatchling build-system

--- radproc/__init__.py
+++ radproc/__init__.py
@@ -1,6 +1,5 @@
 """radar data processing tools"""
-
-__version__ = '0.0.1'
+from radproc._version import __version__
 
 NAN_REPLACEMENT = {'ZH': -10, 'ZDR': 0, 'KDP': 0, 'RHO': 0, 'DP': 0,
                    'PHIDP': 0, 'MLI': 0, 'T':-999}
