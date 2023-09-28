5e06500f75263321fd95471a0c1ec2ec4fac9fb4
Jussi Tiira <jussi.tiira@fmi.fi>
2023-09-28 13:41:40 +0300

    file_field_names=True by default

--- src/radproc/io.py
+++ src/radproc/io.py
@@ -2,7 +2,7 @@
 import pyart.aux_io
 
 
-def read_h5(filename, exclude_datasets=['dataset13'], **kws):
+def read_h5(filename, exclude_datasets=['dataset13'], file_field_names=True, **kws):
     """pyart read_odim_h5 wrapper
 
     Currently, pyart only supports uniform binsize for odim h5.
@@ -12,12 +12,14 @@ def read_h5(filename, exclude_datasets=['dataset13'], **kws):
     try:
         return pyart.aux_io.read_odim_h5(filename,
                                          exclude_datasets=exclude_datasets,
+                                         file_field_names=file_field_names,
                                          **kws)
     except ValueError:
         # second guesses
         for opt in opts:
             try:
                 return pyart.aux_io.read_odim_h5(filename,
+                                                 file_field_names=file_field_names,
                                                  exclude_datasets=opt, **kws)
             except ValueError:
                 continue
